5c34d55a6712cb7c345c05a2e5f9f5f12ce1ae09
Gnilane95 <agathegnilanediouf@gmail.com>
2022-06-20 10:58:03 +0200

    Delate and done ok

--- js/script.js
+++ js/script.js
@@ -9,11 +9,30 @@ document.getElementById("push").onclick = function() {
     } else if (elInput.length > 35) {
         alert ("La tâche est trop longue")
     } else{
-        console.log ("Tout est ok pour envoyer")
-        document.querySelector("#task").innerHTML += `<div class="task d-flex justify-content-between mb-3">
-        <p class="mb-0">${elInput}</p>
-        <button class="btn-danger">Delate</button>
+        // console.log ("Tout est ok pour envoyer")
+        // ajouter une tâche
+        document.querySelector("#task").innerHTML += 
+        `<div class="task d-flex justify-content-between mb-3 border-bottom py-3">
+        <Span class="mb-0 taskname">${elInput}</Span>
+        <button class="btn-danger delate">Delate</button>
         </div>`;
+        // delate task
+        let tasksDelate= document.querySelectorAll(".delate");
+        console.log (tasksDelate)
+        for (let i = 0; i < tasksDelate.length; i++) {
+        tasksDelate [i].onclick= function () {
+            console.log ('click delate');
+            this.parentNode.remove();
+         }
+        }
+        // task done
+        let taskdone = document.querySelectorAll(".taskname");
+        for (let i = 0; i < taskdone.length; i++) {
+           taskdone [i].onclick = function () {
+            this.classList.toggle("completed");
+           };
+        }
+        document.querySelector ("#todo-input").value = ""
     }
 }
 
